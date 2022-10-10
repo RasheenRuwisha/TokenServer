b3003239a06920d7f044bc8b46da1553f2d9d016
Rasheen Ruwisha <rasheenruwisha@Rasheens-MBP.ht.home>
2022-10-10 18:38:22 -0400

    pay

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ app.use(bodyParser.json());
 
 const stripe = require('stripe')('sk_test_UErVLRDhFKCtj3Zj5fU7DKT8008mCYPvRt');
 
-app.post('/api/doPayment/', (req, res) => {
-  return stripe.charges
-    .create({
-      amount: req.body.amount, // Unit: cents
-      currency: 'eur',
-      source: req.body.tokenId,
-      description: 'Test payment',
-    })
-    .then(result => res.status(200).json(result));
+app.post('/api/doPayment/', async (req, res) => {
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: req.body.amount,
+        currency: 'usd',
+      });
+      return paymentIntent.client_secret
 });
 
 app.listen(5000);
