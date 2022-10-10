ec1e05c576c62389c841ce856bda7550d4b59ed3
Rasheen Ruwisha <rasheenruwisha@Rasheens-MBP.ht.home>
2022-10-10 18:06:33 -0400

    pay

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
-var express = require('express');
-var {AccessToken} = require('agora-access-token');
-var {Token, Priviledges} = AccessToken;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-var PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+const stripe = require('stripe')('sk_test_UErVLRDhFKCtj3Zj5fU7DKT8008mCYPvRt');
 
-var APP_ID = '4e0370075e574ef0952c98118c284498';
-var APP_CERTIFICATE = 'bb9663d662554cd5950f7587fae91a3e';
-
-var app = express();
-
-function nocache(req, res, next) {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    next();
-}
-
-var generateAccessToken = function (req, resp) {
-    resp.header('Access-Control-Allow-Origin', "*")
-
-    var channel = req.query.channel;
-    if (!channel) {
-        return resp.status(500).json({ 'error': 'channel name is required' });
-    }
-
-    var uid = req.query.uid;
-    if (!uid) {
-        uid = 0;
-    }
-
-    var expiredTs = req.query.expiredTs;
-    if (!expiredTs) {
-        expiredTs = 0;
-    }
-
-    var token = new Token(APP_ID, APP_CERTIFICATE, channel, uid);
-    // typically you will ONLY need join channel priviledge
-    token.addPriviledge(Priviledges.kJoinChannel, expiredTs);
-    return resp.json({ 'token': token.build() });
-};
-
-app.get('/access_token', nocache, generateAccessToken);
-
-app.listen(PORT, function () {
-    console.log('Service URL http://127.0.0.1:' + PORT + "/");
-    console.log('Channel Key request, /access_token?uid=[user id]&channel=[channel name]');
-    console.log('Channel Key with expiring time request, /access_token?uid=[user id]&channel=[channel name]&expiredTs=[expire ts]');
+app.post('/api/doPayment/', (req, res) => {
+  return stripe.charges
+    .create({
+      amount: req.body.amount, // Unit: cents
+      currency: 'eur',
+      source: req.body.tokenId,
+      description: 'Test payment',
+    })
+    .then(result => res.status(200).json(result));
 });
+
+app.listen(5000);
